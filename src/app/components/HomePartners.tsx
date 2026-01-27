07e89057bf8a71360b600e3aae8f166c70bc6986
gathumartins <gathumartins@gmail.com>
2026-01-27 09:47:40 +0300

    created a new partners slider

--- src/app/components/HomePartners.tsx
+++ src/app/components/HomePartners.tsx
@@ -1,16 +1,18 @@
 'use client'
 import React from 'react'
-import dynamic from "next/dynamic";
+import PartnersSliders from './PartnersSliders';
+// import dynamic from "next/dynamic";
 
-const ScrollCarousel = dynamic(
-  () => import("./ScrollCarouselComponent"),
-  { ssr: true },
-);
+// const ScrollCarousel = dynamic(
+//   () => import("./ScrollCarouselComponent"),
+//   { ssr: true },
+// );
 
 function HomePartners() {
   return (
     <section className="bg-white dark:bg-white contPadd py-10 h-[190px]">
-      <ScrollCarousel/>
+      {/* <ScrollCarousel/> */}
+      <PartnersSliders/>
     </section>
   );
 }
---
+++ src/app/components/PartnersSliders.tsx
@@ -0,0 +1,96 @@
+import React from 'react'
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
+import Image from 'next/image';
+
+
+  const partners = [
+    {
+      id: 1,
+      name: "Amref Kenya",
+      shortname: "amref",
+    },
+    {
+      id: 2,
+      name: "Automation Anywhere",
+      shortname: "automation",
+    },
+    {
+      id: 3,
+      name: "British Council",
+      shortname: "bc",
+    },
+    {
+      id: 4,
+      name: "Christian Aid",
+      shortname: "christianaid",
+    },
+    {
+      id: 5,
+      name: "Lenovo",
+      shortname: "lenovo",
+    },
+    {
+      id: 6,
+      name: "London School of Economic",
+      shortname: "lse",
+    },
+    {
+      id: 7,
+      name: "Malala Foundation",
+      shortname: "malala",
+    },
+    {
+      id: 8,
+      name: "Microsoft",
+      shortname: "microsoft",
+    },
+    {
+      id: 9,
+      name: "Rutgers",
+      shortname: "rutgers",
+    },
+    {
+      id: 10,
+      name: "Team4Tech",
+      shortname: "team4tech",
+    },
+  ];
+function PartnersSliders() {
+  return (
+    <Carousel
+      opts={{
+        loop: true,
+      }}
+      plugins={[
+        Autoplay({
+          delay: 2000,
+        }),
+      ]}
+    >
+      <CarouselContent>
+        {partners.map((partner) => (
+          <CarouselItem
+            key={partner.id}
+            className="h-[110px] basis-1/2 sm:basis-1/3 md:basis-1/4 lg:basis-1/5 xl:basis-1/6 flex justify-center"
+          >
+            <Image
+              src={`/images/${partner.shortname}.svg`}
+              alt={partner.name}
+              width={149}
+              height={107}
+              priority
+              className="dark:grayscale-100 inline-block rounded border-2 border-gray"
+            />
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+    </Carousel>
+  );
+}
+
+export default PartnersSliders
