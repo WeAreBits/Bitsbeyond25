eab54bac20f4282031631e6d7b4b77c66f4f7d3f
gathumartins <gathumartins@gmail.com>
2026-01-30 13:29:17 +0300

    updating the single program page

--- src/app/(frontend)/(WhatWeDo)/programs/[clusterID]/[programID]/page.tsx
+++ src/app/(frontend)/(WhatWeDo)/programs/[clusterID]/[programID]/page.tsx
@@ -6,9 +6,10 @@ function page() {
     <>
       <PagesHero />
       <main className="myCont">
-        <section className="contPadd py-11">
+        <section className="contPadd py-11 bg-white">
           <h2 className="bodyH2 mb-5">Page Heading</h2>
         </section>
+        <section className="contPadd py-11 bg-white"></section>
       </main>
     </>
   );
